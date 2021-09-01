063d77ca0328d97340ddf51cc3db53f36119cae8
JkKU <60291632+Jenrikku@users.noreply.github.com>
2021-09-01 20:36:31 +0200

    Finished 3DW compatibility for standard materials (Wii U to Switch)

--- BfresLibrary/PlatformConverters/Presets/MaterialConverter3DW.cs
+++ BfresLibrary/PlatformConverters/Presets/MaterialConverter3DW.cs
@@ -28,6 +28,8 @@ namespace BfresLibrary.PlatformConverters
 
         internal override void ConvertToSwitchMaterial(Material material) 
         {
+            material.SetShaderParameter("alpha_test_value", ShaderParamType.Float, 0.5f);
+
             material.SetRenderInfo("color_blend_alpha_dst_func", BlendFunction[
                   material.RenderState.BlendControl.AlphaDestinationBlend]);
             material.SetRenderInfo("color_blend_alpha_op", BlendCombine[
@@ -38,11 +40,11 @@ namespace BfresLibrary.PlatformConverters
             material.SetRenderInfo("color_blend_const_color", new float[4] { 0, 0, 0, 0 });
 
             material.SetRenderInfo("color_blend_rgb_dst_func", BlendFunction[
-                  material.RenderState.BlendControl.ColorSourceBlend]);
-            material.SetRenderInfo("color_blend_rgb_op", BlendFunction[
                   material.RenderState.BlendControl.ColorDestinationBlend]);
-            material.SetRenderInfo("color_blend_rgb_src_func", BlendCombine[
+            material.SetRenderInfo("color_blend_rgb_op", BlendCombine[
                   material.RenderState.BlendControl.ColorCombine]);
+            material.SetRenderInfo("color_blend_rgb_src_func", BlendFunction[
+                  material.RenderState.BlendControl.ColorSourceBlend]);
 
             material.SetRenderInfo("display_face", GetCullState(material.RenderState));
 
@@ -74,14 +76,14 @@ namespace BfresLibrary.PlatformConverters
 
         Dictionary<GX2.GX2CompareFunction, string> CompareFunction = new Dictionary<GX2.GX2CompareFunction, string>()
         {
-            { GX2.GX2CompareFunction.Always, "always" },
-            { GX2.GX2CompareFunction.Never, "never" },
-            { GX2.GX2CompareFunction.GreaterOrEqual, "gequal" },
-            { GX2.GX2CompareFunction.LessOrEqual, "lequal" },
-            { GX2.GX2CompareFunction.Equal, "equal" },
-            { GX2.GX2CompareFunction.Less, "less" },
-            { GX2.GX2CompareFunction.Greater, "greater" },
-            { GX2.GX2CompareFunction.NotEqual, "noequal" },
+            { GX2.GX2CompareFunction.Always, "Always" },
+            { GX2.GX2CompareFunction.Never, "Never" },
+            { GX2.GX2CompareFunction.GreaterOrEqual, "Gequal" },
+            { GX2.GX2CompareFunction.LessOrEqual, "Lequal" },
+            { GX2.GX2CompareFunction.Equal, "Equal" },
+            { GX2.GX2CompareFunction.Less, "Less" },
+            { GX2.GX2CompareFunction.Greater, "Greater" },
+            { GX2.GX2CompareFunction.NotEqual, "Noequal" },
         };
 
         Dictionary<GX2.GX2BlendFunction, string> BlendFunction = new Dictionary<GX2.GX2BlendFunction, string>()
